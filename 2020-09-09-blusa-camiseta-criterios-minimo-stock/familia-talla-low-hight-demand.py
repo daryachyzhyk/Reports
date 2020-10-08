a49cb7736762077b70d6bfce0d26fda7ebdbb209
daryachyzhyk <d.chyzhyk@lookiero.com>
2020-10-08 17:08:05 +0200

    add calculation distr_relativa and distr_abs

--- 2020-09-09-blusa-camiseta-criterios-minimo-stock/familia-talla-low-hight-demand.py
+++ 2020-09-09-blusa-camiseta-criterios-minimo-stock/familia-talla-low-hight-demand.py
@@ -25,23 +25,6 @@ path_results = ('/home/darya/Documents/Reports/2020-09-09-blusa-camiseta-criteri
 
 file_feedback = ('/home/darya/Documents/Reports/2020-09-09-blusa-camiseta-criterios-minimo-stock/Stock_Situacion.csv')
 
-# family_list = ['ABRIGO', 'BLUSA', 'CAMISETA', 'CARDIGAN', 'FALDA', 'DENIM', 'JERSEY', 'JUMPSUIT', 'PANTALON', 'PARKA',
-#                'SUDADERA', 'TOP', 'TRENCH', 'VESTIDO']
-#
-#
-# # 'reference', 'category'
-# date_list = ['2020-07-24', '2020-07-27', '2020-07-28', '2020-07-29', '2020-07-30', '2020-07-31', '2020-08-03',
-#              '2020-08-04', '2020-08-05', '2020-08-06', '2020-08-10', '2020-08-11', '2020-08-12', '2020-08-13',
-#              '2020-08-14', '2020-08-17', '2020-08-18', '2020-08-19', '2020-08-20', '2020-08-21']
-#
-# date_list_blusa = ['2020-07-27', '2020-07-28', '2020-07-29', '2020-07-31', '2020-08-03',
-#              '2020-08-04', '2020-08-05', '2020-08-06', '2020-08-10', '2020-08-11', '2020-08-12', '2020-08-13',
-#              '2020-08-14', '2020-08-17', '2020-08-18', '2020-08-19', '2020-08-20', '2020-08-21']
-#
-# date_list_camiseta = ['2020-07-24', '2020-07-27', '2020-07-28', '2020-07-29', '2020-07-30', '2020-08-03',
-#              '2020-08-04', '2020-08-05', '2020-08-10', '2020-08-11', '2020-08-12',
-#              '2020-08-14', '2020-08-17', '2020-08-18', '2020-08-19', '2020-08-20']
-
 #######################################
 # feedback
 df_feedback = pd.read_csv(file_feedback, usecols=['Familia', 'Talla', 'Fecha', 'Stock NOK'])
@@ -63,16 +46,10 @@ date_list = list(set(df_feedback['date']))
 #######################################
 # demanda
 
-
-
 query_demanda_text = 'date_ps_done in @date_list and family_desc in @family_list'
 
 df_demanda_raw = pd.read_csv(file_demanda, usecols=['reference', 'date_ps_done', 'family_desc']).query(query_demanda_text)
 reference_list = list(set(df_demanda_raw['reference'].to_list()))
-
-# df_demanda_raw['family_desc'].value_counts(dropna=True)
-
-
 
 df_demanda = df_demanda_raw.groupby(['date_ps_done', 'reference']).size().reset_index(name='demanda')
 
@@ -85,13 +62,11 @@ query_stock_text = 'date in @date_list and reference in @reference_list'
 
 df_stock_raw = pd.read_csv(file_stock).query(query_stock_text)
 
-
 # visible
 df_stock = df_stock_raw.drop(df_stock_raw[(df_stock_raw['es'] == 0) & (df_stock_raw['fr'] == 0) &
                                           (df_stock_raw['gb'] == 0) & (df_stock_raw['pt'] == 0) &
                                           (df_stock_raw['be'] == 0) & (df_stock_raw['lu'] == 0) &
                                           (df_stock_raw['it'] == 0)].index)
-
 
 df_demanda_stock = pd.merge(df_demanda,
                             df_stock[['date', 'real_stock', 'reference']],
@@ -102,44 +77,11 @@ df_demanda_stock['real_stock'] = df_demanda_stock['real_stock'].fillna(0)
 df_demanda_stock['demanda'] = df_demanda_stock['demanda'].fillna(0)
 
 
-
-
-
 ######################################
 # add description of the product
 # product
 
-
-
-var_list = ['reference', 'family_desc', 'size',
-            'brand',
-            'aventurera', 'basico', 'composicion',
-            # 'estilo_boho',	'estilo_casual', 'estilo_clasico', 'estilo_minimal', 'estilo_noche', 'estilo_street',
-            'fit', 'pattern', 'tejido',
-            # 'uso_administrativa', 'uso_eventos', 'uso_noche', 'uso_tiempo_libre', 'uso_working_girl',
-            'color', 'color_group', 'color_category',
-            'price_range_product',
-            # 'has_pattern', 'basico_pattern',
-            'clima', 'uso',
-            'estilo_producto',
-            'acabado']
-
 var_list_aux = ['reference', 'family_desc', 'size']
-# var_list_cat = ['clima', 'aventurera', 'basico', 'fit', 'pattern', 'origen', 'color_group', 'color_category',
-#                 'price_range_product']
-#
-# var_list_opt = [#'climate_cold_weather',	'climate_soft_cold_weather', 'climate_soft_warm_weather',
-#                 #'climate_soft_weather', 'climate_warm_weather',
-#                 'estilo_boho', 'estilo_casual', 'estilo_clasico', 'estilo_minimal', 'estilo_noche', 'estilo_street',
-#                 'uso_administrativa', 'uso_eventos', 'uso_noche', 'uso_tiempo_libre', 'uso_working_girl',
-#                 'estilo_boho',	'estilo_casual', 'estilo_clasico', 'estilo_minimal', 'estilo_noche', 'estilo_street',
-#                 'acabado_acolchado', 'acabado_agradable', 'acabado_arrugado', 'acabado_brillante', 'acabado_calado',
-#                 'acabado_canale', 'acabado_charol', 'acabado_desgastado', 'acabado_drapeado', 'acabado_encerado',
-#                 'acabado_impermeable', 'acabado_jaspeado', 'acabado_metalizado', 'acabado_parches', 'acabado_plisado',
-#                 'acabado_roto', 'acabado_tablas', 'acabado_tornasolado', 'acabado_transparente']
-
-# test = pd.read_csv(file_product)
-# test_list = test.columns
 
 # TODO add 'composicion' , corte, grosor, ligero -> all fields are Nan
 var_list_cat = ['clima',
@@ -148,13 +90,13 @@ var_list_cat = ['clima',
                 'estilo_producto',
                 'fit',
                 'uso',
-                # 'size',
                 # 'pattern',
                 'has_pattern',
                 #'composicion', ??? Nan
                 'origen',
                 'color_group',
-                'color_category', 'price_range_product',
+                'color_category',
+                'price_range_product',
                 # 'tejido',
                 'acabado',
                 'premium'
@@ -184,50 +126,47 @@ df = pd.merge(df_demanda_stock,
 
 df = df[df['family_desc'].isin(family_list)]
 
+
+
+
 # dummies
-df['size_cat'] = df['size']
-var_list_cat.append('size_cat')
 
 
-df = pd.get_dummies(df, columns=var_list_cat) # , prefix='', prefix_sep=''
+
+
+df = pd.get_dummies(df, columns=var_list_cat)
+
+
 
 df['stock_actual'] = df['real_stock']
 df.loc[df['stock_actual'] < df['demanda'], 'stock_actual'] = df['demanda']
 
 
 
-# test = df[(df['family_desc']=='VESTIDO') & (df['date']=='2020-07-24') & (df['size']=='XXXL')]
+
 
 var_group_aux = ['date', 'demanda', 'real_stock', 'family_desc', 'size', 'stock_actual']
 var_group = list(set(df.columns.to_list()) - set(var_group_aux) - set(['reference']))
 
-
-#
-#
-# cols = ['fit_entallado', 'fit_recto', 'fit_holgado', 'fit_oversize']
-# test2 = test[cols].mul(test['demanda'], axis=0).add_suffix('_demanda')
-# test3 = test[cols].mul(test['stock_actual'], axis=0).add_suffix('_st_act')
-# test_var = pd.concat([test[var_group_aux], test2], axis=1)
-# test_var = pd.concat([test_var, test3], axis=1)
-
-
 df_var_pct = pd.DataFrame(columns=['date', 'family_desc', 'size'])
 df_var_pct_col = pd.DataFrame([])
 
+##########################################
 # test
-# df_test = df.copy()
-#
-#
-#
-#
-#
-# df = df_test.copy()
-# df = df[(df['family_desc'] == 'VESTIDO') & (df['date'] == '2020-07-24') & (df['size']=='XXXL')]
-#
-# pd.set_option('display.max_columns', None)
-# df = df[['family_desc', 'size', 'demanda', 'real_stock', 'stock_actual', 'fit_entallado', 'fit_recto', 'fit_holgado', 'fit_oversize', 'date']]
 
-for col in var_group: # var_group: # ['fit_entallado', 'fit_recto', 'fit_holgado', 'fit_oversize']
+
+df_test = df.copy()
+
+df = df_test.copy()
+df = df[(df['family_desc'] == 'VESTIDO') & (df['date'] == '2020-07-24') & (df['size']=='XXXL')]
+
+#pd.set_option('display.max_columns', None)
+df = df[['family_desc', 'size', 'demanda', 'real_stock', 'stock_actual', 'fit_entallado', 'fit_recto', 'fit_holgado', 'fit_oversize', 'date']]
+var_group = ['fit_entallado', 'fit_recto', 'fit_holgado', 'fit_oversize']
+
+####### end test
+
+for col in var_group:
     print(col)
     columns = var_group_aux + [col]
     df_var = df[columns].copy()
@@ -241,44 +180,66 @@ for col in var_group: # var_group: # ['fit_entallado', 'fit_recto', 'fit_holgado
     # stock real, stock appeared in snapshots
     # stock actual, in case when number of items in snapshots is less then in stock_real,
 
+    # porcentaje de demanda de opcion de demanda de variable
+    df_gr[col + '_pct_demanda_demanda'] = df_gr[col + '_demanda'] / df_gr['demanda']
+
+    # porcentaje de demanda de opcion de demanda de variable
+    df_gr[col + '_pct_stock_stock'] = df_gr[col + '_stock_actual'] / df_gr['stock_actual']
+
     # percentage of variable option (option "holgado" of variable "fit") shipped of all real stock (snapshot),
     # could be more then 100%
-    df_gr[col + '_shipped_stock_real_pct'] = df_gr[col + '_demanda'] / df_gr[col + '_stock_real']
+    df_gr[col + '_pct_demanda_stock_real'] = df_gr[col + '_demanda'] / df_gr[col + '_stock_real']
 
     # percentage of variable option (option "holgado" of variable "fit") shipped of all actual stock (snapshot),
     # could be 100% maximum
-    df_gr[col + '_shipped_stock_actual_pct'] = df_gr[col + '_demanda'] / df_gr[col + '_stock_actual']
+    df_gr[col + '_pct_demanda_stock_actual'] = df_gr[col + '_demanda'] / df_gr[col + '_stock_actual']
 
     # percentage of option (option 'holgado') of variable ('fit') stock, could be 100% maximum
-    df_gr[col + '_varstock_pct'] = df_gr[col + '_stock_actual'] / df_gr['stock_actual']
+    df_gr[col + '_pct_varstock'] = df_gr[col + '_stock_actual'] / df_gr['stock_actual']
 
     # percentage of option (option 'holgado') shipped of variable ('fit') stock
 
+    df_gr[col + '_pct_demanda_weight'] = df_gr[col + '_pct_demanda_stock_actual'] * df_gr[col + '_pct_varstock']
+
+    # TODO: distr relativa, absoluta
+    # distr relativa	distr abs
+    # =if (OR(demanda=0, demanda de stock actual > stock actual), 0, 1)
+    df_gr[col + '_distr_relativa'] = np.where((df_gr[col + '_pct_demanda_demanda'] == 0) |
+                                              (df_gr[col + '_pct_demanda_demanda'] < df_gr[col + '_pct_stock_stock']),
+                                              0, 1)
+
+    df_gr[col + '_distr_abs'] = np.where((df_gr[col + '_demanda'] == 0) |
+                                              (df_gr[col + '_pct_demanda_stock_actual'] < 1),
+                                              0, 1)
 
 
-    df_gr[col + '_shipped_weight_pct'] = df_gr[col + '_shipped_stock_actual_pct'] * df_gr[col + '_varstock_pct']
 
     df_gr = df_gr.fillna(0)
 
-    df_gr = df_gr.replace(np.inf, 10.0)
+    df_gr = df_gr.replace(np.inf, 1.0)
 
+    # TODO: añadir nuevas columnas
     df_var_pct = df_var_pct.merge(df_gr[['date', 'family_desc', 'size',
                                          col + '_stock_actual',
-                                         col + '_shipped_stock_real_pct',
-                                         col + '_shipped_stock_actual_pct',
-                                         col + '_varstock_pct',
-                                         col + '_shipped_weight_pct']],
+                                         col + '_pct_demanda_demanda',
+                                         col + '_pct_stock_stock',
+                                         col + '_pct_demanda_stock_real',
+                                         col + '_pct_demanda_stock_actual',
+                                         col + '_pct_varstock',
+                                         col + '_pct_demanda_weight',
+                                         col + '_distr_relativa',
+                                         col + '_distr_abs']],
                                   on=['date', 'family_desc', 'size'],
                                   how='outer')
 
-    # TODO: save as column
+    # save as column
 
     df_temp = df_var_pct[['date', 'family_desc', 'size']]
-    df_temp['varoption_shipped_stock_real_pct'] = df_var_pct[col + '_shipped_stock_real_pct']
-    df_temp['varoption_shipped_stock_actual_pct'] = df_var_pct[col + '_shipped_stock_actual_pct']
+    df_temp['varoption_pct_demanda_stock_real'] = df_var_pct[col + '_pct_demanda_stock_real']
+    df_temp['varoption_pct_demanda_stock_actual'] = df_var_pct[col + '_pct_demanda_stock_actual']
 
-    df_temp['varoption_varstock_pct'] = df_var_pct[col + '_varstock_pct']
-    df_temp['varoption_shipped_weight_pct'] = df_var_pct[col + '_shipped_weight_pct']
+    df_temp['varoption_pct_varstock'] = df_var_pct[col + '_pct_varstock']
+    df_temp['varoption_pct_demanda_weight'] = df_var_pct[col + '_pct_demanda_weight']
 
     df_temp['varoption'] = col
     df_var_pct_col = df_var_pct_col.append(df_temp)
@@ -288,7 +249,7 @@ for col in var_group: # var_group: # ['fit_entallado', 'fit_recto', 'fit_holgado
 df_var_pct = df_var_pct.fillna(0)
 df_var_pct_col = df_var_pct_col.fillna(0)
 
-df_var_pct_col = df_var_pct_col.replace(np.inf, 10.0)
+df_var_pct_col = df_var_pct_col.replace(np.inf, 1.0)
 
 df_var_pct_ps = df_var_pct.merge(df_feedback,
                                  on=['date', 'family_desc', 'size'],
@@ -302,15 +263,16 @@ df_var_pct_ps = df_var_pct_ps.fillna(0)
 df_var_pct_ps = df_var_pct_ps.replace(np.inf, 1)
 
 # TODO merge df_var_pct_col with PS labels
-
+##################################################################################################################
 # save
-df_var_pct_ps.to_csv(os.path.join(path_results, 'date_family_size_var_pct_psfeedback.csv'), index=False)
-df_var_pct_col_ps.to_csv(os.path.join(path_results, 'date_family_size_var_pct_col_psfeedback.csv'), index=False)
 
-aa = df_var_pct_ps.groupby(['family_desc']).agg({'stock_nok': 'mean'})
-
-with open(os.path.join(path_results, 'var_list.txt'), "wb") as fp:  # Pickling
-    pickle.dump(var_group, fp)
+# df_var_pct_ps.to_csv(os.path.join(path_results, 'date_family_size_var_pct_psfeedback.csv'), index=False)
+# df_var_pct_col_ps.to_csv(os.path.join(path_results, 'date_family_size_var_pct_col_psfeedback.csv'), index=False)
+#
+# aa = df_var_pct_ps.groupby(['family_desc']).agg({'stock_nok': 'mean'})
+#
+# with open(os.path.join(path_results, 'var_list.txt'), "wb") as fp:  # Pickling
+#     pickle.dump(var_group, fp)
 
 
 
